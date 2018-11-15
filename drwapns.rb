650688ed058c9cc8fb07a06412c9e276cfa9055e
Darren <donaldwu1101@gmail.com>
2018-11-15 18:52:46 +0800

    edit make build depend on Xcode 10.1

--- drwapns.rb
+++ drwapns.rb
@@ -9,20 +9,10 @@ class Drwapns < Formula
   # depends_on "cmake" => :build
 
   def install
-    system "make", "install" "PREFIX=#{prefix}" # if this fails, try separate make/make install steps
+    system "make", "install", "PREFIX=#{prefix}" # if this fails, try separate make/make install steps
   end
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test DRWApns`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
+ depends_on :xcode => ["10.1", :build]
+
 end
 
