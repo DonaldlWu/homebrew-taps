7e843c2092a29a596930d0ecb7454d09b63376eb
Darren <donaldwu1101@gmail.com>
2018-11-15 18:41:43 +0800

    drwapns v1.0.1

--- drwapns.rb
+++ drwapns.rb
@@ -4,13 +4,32 @@
 class Drwapns < Formula
   desc "A tool for sending push notification to iOS devices"
   homepage ""
-  url "https://github.com/DonaldlWu/DRWApns/archive/v1.0.0.tar.gz"
-  sha256 "b8e3474e0b90aa432fba964a57b60a58c1ff3637e7e9b7665a1c05c080a5d4ff"
+  url "https://github.com/DonaldlWu/DRWApns/archive/v1.0.1.tar.gz"
+  sha256 "c4c5bddbb0b16c5baaa31fcb8cbac10443bb69ea7f0161e846a525d2217b37f3"
   # depends_on "cmake" => :build
 
   def install
-    system "make", "install", "PREFIX=#{prefix}" # if this fails, try separate make/make install steps
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
   end
 
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test DRWApns`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
 end
 
