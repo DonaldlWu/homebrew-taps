344face61dc77813b40b6df8900c667357da605c
Darren <donaldwu1101@gmail.com>
2018-11-16 11:26:14 +0800

    v1.0.3

--- drwapns.rb
+++ drwapns.rb
@@ -2,17 +2,22 @@
 #                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Drwapns < Formula
-    desc "A tool for sending push notification to iOS devices"
-    homepage ""
-    url "https://github.com/DonaldlWu/DRWApns/archive/v1.0.1.tar.gz"
-    sha256 "c4c5bddbb0b16c5baaa31fcb8cbac10443bb69ea7f0161e846a525d2217b37f3"
-    
-    def install
-    system "make"
-    system "install"
-    system "PREFIX=#{prefix}" # if this fails, try separate make/make install steps
+  desc "A tool for sending push notification to iOS devices"
+  homepage ""
+  url "https://github.com/DonaldlWu/DRWApns/archive/v1.0.3.tar.gz"
+  sha256 "5fedd0e90bd66c5c78c856c09f86a401f67199912fa60690de7023c2872dff53"
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
   end
 
-  depends_on :xcode => ["9.2", :build]
 end
 
