bea9edc76ff5f572e809cc4e9342ecde41bcc19f
Darren <donaldwu1101@gmail.com>
2018-11-16 11:43:29 +0800

    v1.0.4 -2

--- drwapns.rb
+++ drwapns.rb
@@ -4,14 +4,14 @@
 class Drwapns < Formula
   desc "A tool for sending push notification to iOS devices"
   homepage ""
-  url "https://github.com/DonaldlWu/DRWApns/archive/v1.0.3.tar.gz"
-  sha256 "5fedd0e90bd66c5c78c856c09f86a401f67199912fa60690de7023c2872dff53"
+  url "https://github.com/DonaldlWu/DRWApns/archive/1.0.4.tar.gz"
+  sha256 "6bd5a44543f8f1c8831d268ba5806e206a6caf7cce55c3146ebd243cfeebffb5"
   # depends_on "cmake" => :build
 
   def install
     system "make", "install", "PREFIX=#{prefix}" # if this fails, try separate make/make install steps
   end
 
-  depends_on :xcode => ["9.2", :build]
+  depends_on "xcode" => ["9.2", :build]
 end
 
