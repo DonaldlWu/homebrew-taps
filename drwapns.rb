fc2b0a4282052beffd9ad3acc9367b155e4d7b10
Darren <donaldwu1101@gmail.com>
2018-11-16 11:29:32 +0800

    v1.0.3 -3

--- drwapns.rb
+++ drwapns.rb
@@ -9,14 +9,7 @@ class Drwapns < Formula
   # depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-#    system "./configure", "--disable-debug",
-#                          "--disable-dependency-tracking",
-#                          "--disable-silent-rules",
-#                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install", "PREFIX=#{prefix}" # if this fails, try separate make/make install steps
   end
 
 end
